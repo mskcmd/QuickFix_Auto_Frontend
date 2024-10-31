4dafea6ab73e27f8dad1144a33d943bcd1a3bd5e
Muhammed Suhail <wwwsuhail883@gmail.com>
2024-10-31 08:52:14 +0530

    fix firebase

--- src/Config/Firebase.tsx
+++ src/Config/Firebase.tsx
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 // Use environment variables for Firebase config
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,7 +11,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
